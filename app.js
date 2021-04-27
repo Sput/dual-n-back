287600ee73b8907474b87d8fdb315a99a1697690
Sput <paulknick@gmail.com>
2021-04-27 16:24:02 -0400

    functions all coded, basic logic in place, everything 80% done, none of it works

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 let past_blocks = [];
 
 function Light_up_new_block(){
-    new_block = Math.floor(Math.random()*9);
-    past_blocks.push(new_block);
+    
     switch (new_block) {
         case 0:
             document.getElementById("top_left").toggleClass('active');
@@ -44,22 +43,47 @@ function getRandomNumber(){
 }
 
 //Function to take in button push
-function buttonPressed(e)
+function buttonPressed(past_blocks, new_block, num_back) {
+    let correctGuess = 0;
+    if (past_blocks[past_blocks.length-num_back] == new_block){
+        correctGuess = 1;
+    }
+    return correctGuess;
+}
 
 //Function to update score data
+function updateScore(correctGuess, current_score) {
+    if (correctGuess){
+        current_score++;
+    }
+    return current_score;
+}
 
-//Function to update the game stage (how many blocks have been shown)
+
+//Function to update the game stage (how many blocks have been shown) 
+//right now this is not a function, not sure if I need to make it one, right now it makes sense
+//to just have one line that reads: rounds_left = rounds_left--;
 
 //Update score and game state on the screen
-
-
+function updatestoScreen(current_score) {
+    let score = document.getElementById("current_score");
+    score.innerHTML = current_score;
+    return;
+}
 
 
 document.getElementById("begin").onclick = function(){
     //Light up a new block
     //console.log(Math.floor(Math.random()*9));
+    let rounds_left = 20;
+    let num_back = document.getElementById("num_back").value; 
     console.log("begin button pressed");
-    document.addEventListener("keypress", buttonPressed)
+    let new_block = getRandomNumber();
+    past_blocks.push(new_block);
+    userGuess = document.getElementById("match_btn").addEventListener("click", buttonPressed(past_blocks, new_block, num_back))
+    current_score = updateScore(userGuess, current_score);
+    rounds_left = rounds_left--;
+    updatestoScreen(current_score);
     Light_up_new_block();
     console.log(past_blocks)
 };
